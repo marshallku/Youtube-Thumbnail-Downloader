84df1d3ca700989d7d580d7ead9a710a7d809c00
Marshall Ku <taengoo890309@naver.com>
2019-06-01 13:08:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-$(".button").click(function() {
-  $(".ytdwnld, .ytpreveal-img").empty(),
-    $("#result-wrapper").show(),
-    $(".ytdwnld").append("<span class='download'><a href='https://i.ytimg.com/vi/" + $("#ytinput").val().replace(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?/g, "") + "/original.jpg' target='_blank'>Download</a></span>"),
-    $(".ytpreveal-img").append("<img src='https://i.ytimg.com/vi/" + $("#ytinput").val().replace(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?/g, "") + "/original.jpg'>")
+const dwnld = document.querySelector(".ytdwnld"),
+  preveal = document.querySelector(".ytpreveal-img"),
+  a = document.createElement("a"),
+  span = document.createElement("span"),
+  img = document.createElement("img");
+let newSrc;
+document.querySelector(".button").addEventListener("click", function() {
+  const input = document.getElementById("ytinput").value;
+  input !== "" && (
+    newSrc = `https://i.ytimg.com/vi/${input.replace(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?/g, "")}/original.jpg`,
+    dwnld.innerHTML = "",
+    preveal.innerHTML = "",
+    document.getElementById("result-wrapper").style.display = "block",
+    span.classList.add("download"),
+    a.href = newSrc,
+    a.target = "_blank",
+    a.download = "thumbnail.jpg",
+    a.innerText = "download",
+    span.append(a),
+    dwnld.append(span),
+    img.src = newSrc,
+    preveal.append(img)
+  )
 })
