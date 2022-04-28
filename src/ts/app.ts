7abe3a43f19a260941e930d56e53067f063fcd60
marshallku <marshall@kakao.com>
2022-04-28 21:42:46 +0900

    Fix type assertion

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,7 +3,9 @@ import "../css/style.css";
 
 (function () {
     const result = document.querySelector(".result") as HTMLElement;
-    const thumbnailContainer = document.querySelector(".result__thumbnail");
+    const thumbnailContainer = document.querySelector(
+        ".result__thumbnail"
+    ) as HTMLElement;
     const thumbnails = ["original", "maxresdefault", "hqdefault", "mqdefault"];
 
     const renderThumbnail = (src: string, index: number) => {
@@ -26,12 +28,12 @@ import "../css/style.css";
 
         img.src = src;
 
-        if (result!.style.display !== "block") {
-            result!.style.display = "block";
+        if (result.style.display !== "block") {
+            result.style.display = "block";
         }
 
-        thumbnailContainer!.innerHTML = "";
-        thumbnailContainer!.append(img);
+        thumbnailContainer.innerHTML = "";
+        thumbnailContainer.append(img);
     };
 
     document.querySelector(".form")!.addEventListener("submit", (event) => {
