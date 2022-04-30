6d872bb46783759375129622c2a13d4005bcc67e
marshall-ku <marshall@kakao.com>
2022-05-01 00:34:22 +0900

    Add test cases

---
+++ src/test/youtube.test.ts
@@ -0,0 +1,25 @@
+import { getIdFromUri } from "../ts/utils/youtube";
+
+describe("Get ID from URI", () => {
+    it("Get ID from video", () => {
+        expect(
+            getIdFromUri("https://www.youtube.com/watch?v=H69tJmsgd9I")
+        ).toBe("H69tJmsgd9I");
+        expect(getIdFromUri("https://youtu.be/H69tJmsgd9I")).toBe(
+            "H69tJmsgd9I"
+        );
+    });
+    it("Get ID from shorts", () => {
+        expect(getIdFromUri("https://www.youtube.com/shorts/9VtlV9ed0eg")).toBe(
+            "9VtlV9ed0eg"
+        );
+    });
+    it("Get ID from uri with query", () => {
+        expect(
+            getIdFromUri("https://www.youtube.com/watch?v=H69tJmsgd9I&t=0s")
+        ).toBe("H69tJmsgd9I");
+        expect(
+            getIdFromUri("https://youtube.com/shorts/9VtlV9ed0eg?feature=share")
+        ).toBe("9VtlV9ed0eg");
+    });
+});
